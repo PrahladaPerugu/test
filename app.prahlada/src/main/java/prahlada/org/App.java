4f49f9962d0a0c143fd7891dad888f0eeffbde95
root <root@ip-172-31-66-231.ec2.internal>
2018-03-26 07:03:38 +0000

    maven build project

---
+++ app.prahlada/src/main/java/prahlada/org/App.java
@@ -0,0 +1,13 @@
+package prahlada.org;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
